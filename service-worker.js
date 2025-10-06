8d6d5675692339b4f8e431940585ac17fb59d1a4
JiyadR <jiyadrifqipasaribu@gmail.com>
2025-10-06 12:15:53 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,12 @@
 const CACHE_NAME = "webjivad-cache-v2";
 const urlsToCache = [
-  "/",
-  "/Tugas_PWA_Jiyad/index.html",
-  "/Tugas_PWA_Jiyad/about.html",
-  "/Tugas_PWA_Jiyad/contact.html",
-  "/Tugas_PWA_Jiyad/offline.html",
-  "/Tugas_PWA_Jiyad/manifest.json",
+  "./index.html",
+  "./about.html",
+  "./contact.html",
+  "./offline.html",
+  "./manifest.json",
+  "./bacground/me.png",
+  "./bacground/bg unand 2.png"
 ];
 
 // Install Service Worker
@@ -21,37 +22,27 @@ self.addEventListener("install", (event) => {
 // Activate Service Worker & hapus cache lama
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
         cacheNames
           .filter((name) => name !== CACHE_NAME)
           .map((name) => caches.delete(name))
-      );
-    })
+      )
+    )
   );
 });
 
+const offlinePage = "/Tugas_PWA_Jiyad/offline.html";
+
 // Fetch handler
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        return response;
-      })
-      .catch(() => {
-        // Jika gagal ambil dari cache
-        return caches.match(event.request).then((cachedResponse) => {
-          if (cachedResponse) {
-            return cachedResponse;
-          }
-          // Jika request dokumen HTML dan offline
-          if (
-            event.request.mode === "navigate" ||
-            event.request.destination === "document"
-          ) {
-            return caches.match("/Tugas_PWA_Jiyad/offline.html");
-          }
-        });
-      })
-  );
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match(offlinePage))
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => cachedResponse || fetch(event.request))
+    );
+  }
 });
